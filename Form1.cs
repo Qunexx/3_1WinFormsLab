46c75eb0cc2d0c881a52913a87ae6e9811f08bed
Andrey <YtrikPlay@gmail.com>
2023-04-23 16:53:38 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winformlab3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double a;
+        private double b;
+        private MyCalc MyCalc1 = new MyCalc(); //Занимаем место для нового класса
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            
+            
+                try
+                {
+                    MyCalc1.a = a;
+                    MyCalc1.b = b;
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Ошибка: " + exception.Message);
+                    textBox1.Focus();
+                    return;
+                }
+
+                MessageBox.Show("Параметры считаны!");
+            
+
+            //Вызов метода для расчета
+            double Rez = MyCalc1.calc();
+
+            textBox3.Text = Rez.ToString("F4");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ calc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace winformlab3_1
+{
+    public class MyCalc
+    {
+            private double _a = 0.0;
+            private double _b = 0.0;
+
+            public double a
+            {
+                get { return _a;} 
+                set
+                {
+                    if (value == 0) 
+                        throw new Exception("Нулевое значение a недопустимо!");
+                    else
+                        _a = value;
+                }
+            }
+
+            public double b
+            {
+                get { return _b; }
+                set
+                {
+                    if (value == 0) 
+                        throw new Exception("Нулевое значение b недопустимо!");
+                    else
+                        _b = value;
+                }
+            }
+
+            public double calc()
+            {
+                return ((a+b)/a+ (a - b)/b);
+            }
+            
+        }
+    }
